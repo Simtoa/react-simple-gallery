53ba80e3e22a383d1f9f9a1b8421eff6ae9c504f
Simto Alev <simto@core-attacks.com>
2020-08-15 22:53:44 +0300

    Prettier Settings eklendi. VS Code içinde vardı ama neen projede de olmasın?

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React from 'react'
 
-import styles from "./Header.module.css";
+import styles from './Header.module.css'
 
 const Header = () => {
   return (
@@ -8,7 +8,7 @@ const Header = () => {
       <h1>LOggO</h1>
       <p>Lorem ipsum dolor sit amet consectetur.</p>
     </div>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
