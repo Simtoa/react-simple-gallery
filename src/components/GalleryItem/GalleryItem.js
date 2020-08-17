24126a47ccf844fcf1812c56d0199913e5af1906
Simto Alev <simto@core-attacks.com>
2020-08-17 23:01:54 +0300

    Basit Stillendirme yapıldı.

--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
+import styles from './GalleryItem.module.css'
+
 const GalleryItem = ({url, thumbnailUrl}) => {
   return (
-    <a href={url}>
-      <img src={thumbnailUrl} alt="" />
-    </a>
+    <div className={styles.galleryItem}>
+      <a href={url}>
+        <img src={thumbnailUrl} alt="" />
+      </a>
+    </div>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import 'css/normalize.css'
+import 'css/simtonit.css'
+import 'css/app.css'
+
 //Import Components
 import Header from "./components/header/Header";
 import Gallery from "./components/Gallery/Gallery";
